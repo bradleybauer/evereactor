84a383e1b63b7cd13f991e710cf1e4ceb6e1d262
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-03-29 22:03:17 -0400

    Build web code for drift, again.

--- generate_code.sh
+++ generate_code.sh
@@ -1,11 +1,13 @@
+# FOR DEBUG MODE
 rm -rf build
 mkdir build
 
 flutter pub run build_runner build --delete-conflicting-outputs -o web:build/web/
 cp -f build/web/cacheDbWorker.dart.js web/cacheDbWorker.dart.js
 
-rm -rf build
-mkdir build
-
-flutter pub run build_runner build --release --delete-conflicting-outputs -o web:build/web/
-cp -f build/web/cacheDbWorker.dart.js web/cacheDbWorker.dart.min.js
+# FOR RELEASE MODE
+# rm -rf build
+# mkdir build
+# 
+# flutter pub run build_runner build --release --delete-conflicting-outputs -o web:build/web/
+# cp -f build/web/cacheDbWorker.dart.js web/cacheDbWorker.dart.min.js
