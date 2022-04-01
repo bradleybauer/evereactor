4ef7d1c194dc69e3e7734e2928140625d2569cab
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-04-01 17:17:47 -0400

    Rename file.

--- lib/cache_database/platform.dart
+++ lib/cache_database/platform.dart
@@ -1,6 +1,6 @@
 import 'package:drift/drift.dart';
 
-import 'platform_stub.dart' if (dart.library.io) 'platform_app.dart' if (dart.library.html) 'platform_web.dart';
+import 'platform_stub.dart' if (dart.library.io) 'platform_desktop.dart' if (dart.library.html) 'platform_web.dart';
 
 class Platform {
   static QueryExecutor createDatabaseConnection(String databaseName) => PlatformInterface.createDatabaseConnection(databaseName);
