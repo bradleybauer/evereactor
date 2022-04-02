c015fb23fe77d36b1b761d682f4e274c219670ab
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-04-01 21:04:09 -0400

    Align widget to the top-center of the browser window and add some padding around it.

--- lib/gui/widgets/home_page.dart
+++ lib/gui/widgets/home_page.dart
@@ -4,6 +4,7 @@ import 'content.dart';
 import '../my_theme.dart';
 import 'header.dart';
 import 'footer.dart';
+import '../../platform.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage(this.maxHeight, this.width, {Key? key}) : super(key: key);
@@ -16,29 +17,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var platformSpecificPadding = const EdgeInsets.all(0);
+    if (Platform.isWeb()) {
+      platformSpecificPadding = const EdgeInsets.all(32);
+    }
     return Material(
       color: theme.colors.background,
       child: Align(
-        alignment: Alignment.center,
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          clipBehavior: Clip.antiAlias,
-          child: Container(
-            color: theme.colors.primaryContainer,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Header(height: headerHeight, width: width),
-                ConstrainedBox(
-                  constraints:
-                      BoxConstraints(minHeight: 0, maxHeight: maxHeight - headerHeight - footerHeight, maxWidth: width, minWidth: width),
-                  child: SingleChildScrollView(
-                    child: Content(width: width),
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: platformSpecificPadding,
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            clipBehavior: Clip.antiAlias,
+            child: Container(
+              color: theme.colors.primaryContainer,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Header(height: headerHeight, width: width),
+                  ConstrainedBox(
+                    constraints:
+                        BoxConstraints(minHeight: 0, maxHeight: maxHeight - headerHeight - footerHeight, maxWidth: width, minWidth: width),
+                    child: SingleChildScrollView(
+                      child: Content(width: width),
+                    ),
                   ),
-                ),
-                Footer(height: footerHeight, width: width),
-              ],
+                  Footer(height: footerHeight, width: width),
+                ],
+              ),
             ),
           ),
         ),
