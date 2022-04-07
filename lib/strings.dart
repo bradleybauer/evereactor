f80aa1d7dcce5097592c8a939542a2ce6a8b01b7
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-04-07 17:52:39 -0400

    Add string localization data class.

---
+++ lib/strings.dart
@@ -0,0 +1,22 @@
+abstract class Strings {
+  static const Set<String> langs = {
+    'en', // english
+    'zh', // chinese
+  };
+  static String _lang = 'en';
+  static void setLang(String lang) {
+    _lang = lang;
+  }
+
+  static String _get(final Map<String, String> map) {
+    if (map.containsKey(_lang)) {
+      return map[_lang]!;
+    }
+    return map['en']!;
+  }
+
+  static const Map<String, String> _pasteInventory = {
+    'en': 'Paste Inventory',
+  };
+  static String get pasteInventory => _get(_pasteInventory);
+}
