02229ad1c62027ced9a06f6fbdd9187d58415c80
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-04-07 13:37:22 -0400

    Allow open state of flyout to be controlled by multiple sources using boolean OR between each source.

--- lib/gui/widgets/flyout_controller.dart
+++ lib/gui/widgets/flyout_controller.dart
@@ -5,27 +5,42 @@ class FlyoutController extends ChangeNotifier {
   FlyoutController(this.closeTimeout);
 
   final Duration closeTimeout;
+
+  // Allow open state of flyout to be controlled by multiple sources using boolean OR between each source.
+  int _keepOpenVotes = 0;
+
   Timer? _closeTimer;
   bool _isOpen = false;
 
   bool get isOpen => _isOpen;
 
   void open() {
+    _keepOpenVotes += 1;
     _isOpen = true;
     _closeTimer?.cancel();
     _closeTimer = null;
     notifyListeners();
   }
 
+  void _closeNoVote() {
+    if (_keepOpenVotes <= 0) {
+      assert(_keepOpenVotes == 0);
+      _keepOpenVotes = 0;
+      _isOpen = false;
+      _closeTimer?.cancel();
+      _closeTimer = null;
+      notifyListeners();
+    }
+  }
+
   void close() {
-    _isOpen = false;
-    _closeTimer?.cancel();
-    _closeTimer = null;
-    notifyListeners();
+    _keepOpenVotes -= 1;
+    _closeNoVote();
   }
 
   void startCloseTimer() {
+    _keepOpenVotes -= 1;
     _closeTimer?.cancel();
-    _closeTimer = Timer(closeTimeout, close);
+    _closeTimer = Timer(closeTimeout, _closeNoVote);
   }
 }
