84289afc209c70b8d8e82cb1ad8ac743d35c3d58
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-04-08 01:32:17 -0400

    Use ListView.builder in search content for performance reasons. Still have performance issues.

--- lib/gui/widgets/search_bar_flyout_content.dart
+++ lib/gui/widgets/search_bar_flyout_content.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
 import 'package:EveIndy/gui/widgets/hover_button.dart';
 import 'package:flutter/material.dart';
 
@@ -15,88 +13,88 @@ class SearchBarFlyoutContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
-      constraints: BoxConstraints(minWidth: 400, maxWidth: 400, minHeight: 75, maxHeight: 400),
+      constraints: const BoxConstraints(minWidth: 400, maxWidth: 400, minHeight: 75, maxHeight: 400),
       child: PhysicalModel(
         color: Colors.white,
         elevation: 2,
         borderRadius: BorderRadius.circular(4),
         clipBehavior: Clip.antiAliasWithSaveLayer,
         child: Container(
-          color: theme.colors.tertiaryContainer,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 4),
-              child: DataTable(
-                dataTextStyle: TextStyle(fontFamily: 'NotoSans', fontSize: 11, color: theme.colors.onBackground),
-                headingTextStyle:
-                    TextStyle(fontFamily: 'NotoSans', fontSize: 13, fontWeight: FontWeight.w700, color: theme.colors.onBackground),
-                showCheckboxColumn: false,
-                dividerThickness: .000001,
-                dataRowHeight: 24,
-                columnSpacing: 8,
-                horizontalMargin: 8,
-                headingRowHeight: 38,
-
-                ///(add, icon, name, profit %)
-                columns: [
-                  DataColumn(label: Row(children: [SizedBox(width: 32), Text('Item')]), onSort: (i, b) {}),
-                  DataColumn(label: const Text('Profit %'), onSort: (i, b) {}),
-                ],
-                rows: [
-                  DataRow(cells: [
-                    DataCell(Row(
-                      children: [
-                        HoverButton(
-                          builder: (hovered) {
-                            return Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Icon(Icons.add, size: 11, color: hovered ? theme.colors.onTertiary : theme.colors.onBackground));
-                          },
-                          borderRadius: 4,
-                          hoveredElevation: 0,
-                          color: theme.colors.background,
-                          hoveredColor: theme.colors.tertiary,
-                          splashColor: theme.colors.onTertiary.withOpacity(.5),
-                          onTap: () {},
+            color: theme.colors.tertiaryContainer,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                PhysicalModel(
+                  color: Colors.transparent,
+                  elevation: 1,
+                  child: Container(
+                    color: theme.colors.tertiaryContainer,
+                    child: Row(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Item",
+                          style: TextStyle(
+                              fontFamily: 'NotoSans', fontSize: 13, fontWeight: FontWeight.w700, color: theme.colors.onTertiaryContainer),
                         ),
-                        SizedBox(width: MyTheme.appBarPadding),
-                        Text(
-                          'Reaction-Orienting Neurolink Stabilizer',
-                          style: TextStyle(fontFamily: 'NotoSans', fontSize: 11, color: theme.colors.onTertiaryContainer),
+                      ),
+                      Expanded(child: Container()),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Profit %",
+                          style: TextStyle(
+                              fontFamily: 'NotoSans', fontSize: 13, fontWeight: FontWeight.w700, color: theme.colors.onTertiaryContainer),
                         ),
-                      ],
-                    )),
-                    DataCell(Text('32%')),
-                  ]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                  DataRow(cells: [DataCell(Text('11')), DataCell(Text('hai'))]),
-                ],
-              ),
-            ),
-          ),
-        ),
+                      ),
+                    ]),
+                  ),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                        addAutomaticKeepAlives: true,
+                        itemCount: 100,
+                        itemBuilder: (_, index) {
+                          print('Building $index');
+                          return Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                                child: HoverButton(
+                                  builder: (hovered) {
+                                    return Padding(
+                                        padding: const EdgeInsets.all(2.0),
+                                        child: Icon(Icons.add, size: 11, color: hovered ? theme.colors.onTertiary : theme.colors.onBackground));
+                                  },
+                                  borderRadius: 4,
+                                  hoveredElevation: 0,
+                                  color: theme.colors.background,
+                                  hoveredColor: theme.colors.tertiary,
+                                  splashColor: theme.colors.onTertiary.withOpacity(.5),
+                                  onTap: () {
+                                    print('Adding $index');
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                width: 300,
+                                child: Text(
+                                  'Reaction-Orienting Neurolink Stabilizer',
+                                  style: TextStyle(fontFamily: 'NotoSans', fontSize: 11, color: theme.colors.onTertiaryContainer),
+                                ),
+                              ),
+                              SizedBox(width: MyTheme.appBarPadding),
+                              Text(
+                                '32%',
+                                style: TextStyle(fontFamily: 'NotoSans', fontSize: 11, color: theme.colors.onTertiaryContainer),
+                              )
+                            ],
+                          );
+                        })),
+              ],
+            )),
       ),
     );
   }
