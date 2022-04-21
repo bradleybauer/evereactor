1c425dbda625b101b54e07dcfb228a3b86bc2239
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-04-20 22:36:21 -0400

    Think about how to make async work nice with 'as you type' computations.

--- lib/adapters/build.dart
+++ lib/adapters/build.dart
@@ -1,42 +1,89 @@
-import 'package:EveIndy/models/build_options.dart';
 import 'package:flutter/material.dart';
 
-import '../models/build_items.dart';
-import '../models/inventory.dart';
+import 'build_items.dart';
+import 'build_options.dart';
+import 'inventory.dart';
 
-class Build with ChangeNotifier{
-  Inventory inventory;
-  BuildOptions buildOptions;
-  BuildItems buildItems;
+class Build with ChangeNotifier {
+  InventoryAdapter inventory;
+  BuildOptionsAdapter buildOptions;
+  BuildItemsAdapter buildItems;
 
-  Build(this.inventory, this.buildOptions, this.buildItems);
+  Build(this.inventory, this.buildOptions, this.buildItems) {
+    buildItems.addListener(_handleBuildChanged);
+    buildOptions.addListener(_handleBuildChanged);
+    inventory.addListener(_handleBuildChanged);
+  }
 
-  void add(int tid, int runs) {
-    buildItems.add(tid, runs);
+  void _handleBuildChanged() {
+    // update schedule
     notifyListeners();
   }
 
-  void remove(int tid) {
-    buildItems.remove(tid);
-    notifyListeners();
-  }
+  void add(int tid, int runs) => buildItems.add(tid, runs);
+
+  void remove(int tid) => buildItems.remove(tid);
 }
 
-
 // buildAdapter
-//   -inventoryAdapter
-//   -buildOptionsAdapter
-//   -buildItemsAdapter - runs/buildOrBuy/BpOps
 //   get multi-buy
 //   get total build time
 //   get total output volume
 //   get output volume (tid)
 //   get bom
-//   "get schedule"
-//   "get output info"
+//   "get product info"
 //   "get build string"
 //   set/clear inv
-//   add/remove items (mutates builditems)
 //
 // targets table
 //  get rows
+
+/*
+class ChainProcessor {
+  var _arg = '';
+  var didUpdateArg = false;
+  bool isComputing = false;
+
+  Future<void> _computation(arg) async {
+    print('Computation('+arg+') start');
+    await Future.delayed(const Duration(seconds: 3));
+    print('Computation('+arg+') done');
+  }
+
+  void chain() async {
+    do {
+      didUpdateArg = false;
+      await _computation(_arg);
+    } while (didUpdateArg);
+    isComputing = false;
+  }
+
+  void compute(nextarg) {
+    _arg = nextarg;
+    if (!isComputing) {
+      isComputing = true;
+      chain();
+    } else {
+      didUpdateArg = true;
+    }
+  }
+}
+
+Future<void> main() async {
+  final processor = ChainProcessor();
+
+  // initial computation request
+  processor.compute('1');
+
+  // new computation requests with different arguments
+  Future.delayed(const Duration(seconds: 1), () => processor.compute('2'));
+  Future.delayed(const Duration(milliseconds: 1500), () => processor.compute('3'));
+
+  // but only the argument in the most recent request is computed
+  Future.delayed(const Duration(milliseconds: 1600), () => processor.compute('4'));
+
+  // when no computation is being done, more chains can be started
+  Future.delayed(const Duration(seconds: 11), () => processor.compute('5'));
+  Future.delayed(const Duration(seconds: 12), () => processor.compute('6'));
+}
+ */
