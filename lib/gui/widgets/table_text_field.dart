4b2f1bd2ec16dccdee6097fb0229cdfb844170be
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-04-24 18:06:15 -0400

    Add good formatters (float / int) to the text field.

--- lib/gui/widgets/table_text_field.dart
+++ lib/gui/widgets/table_text_field.dart
@@ -17,6 +17,7 @@ class TableTextField extends StatefulWidget {
     this.hintText = '',
     this.textAlign = TextAlign.right,
     this.allowEmptyString = false,
+    this.floatingPoint = false,
     this.hintStyle,
   }) : super(key: key);
 
@@ -33,6 +34,7 @@ class TableTextField extends StatefulWidget {
   final TextStyle? hintStyle;
   final void Function(String) onChanged;
   final bool allowEmptyString;
+  final bool floatingPoint;
 
   @override
   State<TableTextField> createState() => _TableTextFieldState();
@@ -49,9 +51,86 @@ class _TableTextFieldState extends State<TableTextField> {
 
   @override
   void didUpdateWidget(TableTextField oldWidget) {
-    controller.text = widget.initialText;
-    controller.notifyListeners();
+    if (shouldReplaceControllerTextWithWidgetText()) {
+      controller.text = widget.initialText;
+      controller.selection = TextSelection.fromPosition(TextPosition(offset: controller.text.length));
+      controller.notifyListeners();
+    }
     super.didUpdateWidget(oldWidget);
+  }
+
+  bool shouldReplaceControllerTextWithWidgetText() {
+    bool shouldUpdate = controller.text == '';
+    if (!shouldUpdate) {
+      try {
+        final parser = widget.floatingPoint ? double.parse : int.parse;
+        final num controllerValue = parser(controller.text);
+        final num widgetValue = parser(widget.initialText);
+        shouldUpdate |= controllerValue != widgetValue;
+      } catch (e) {}
+    }
+    return shouldUpdate;
+  }
+
+  String removeLeadingCharacterAndSubsequenZeros(String str) {
+    var ret = str.substring(1);
+    while (ret[0] == '0' && ret.length > 1) {
+      ret = ret.substring(1);
+    }
+    if (ret[0] == '0') {
+      ret = '';
+    }
+    return ret;
+  }
+
+  TextEditingValue intFormatter(TextEditingValue oldValue, TextEditingValue newValue) {
+    if (widget.allowEmptyString && newValue.text == '') {
+      return newValue;
+    }
+    try {
+      final text = newValue.text;
+      if (text.startsWith('0')) {
+        final ret = removeLeadingCharacterAndSubsequenZeros(text);
+        if (ret == '') {
+          return oldValue;
+        } else {
+          return TextEditingValue(text: ret, selection: TextSelection.fromPosition(TextPosition(offset: ret.length)));
+        }
+      }
+      // weird i know
+      if (text.length == widget.maxNumDigits + 1) {
+        final ret = removeLeadingCharacterAndSubsequenZeros(text);
+        return TextEditingValue(text: ret, selection: TextSelection.fromPosition(TextPosition(offset: ret.length)));
+      }
+      if (text.length > widget.maxNumDigits) {
+        return oldValue;
+      }
+      if (text.isNotEmpty) {
+        int value = int.parse(text);
+      }
+      return newValue;
+    } catch (e) {}
+    return oldValue;
+  }
+
+  TextEditingValue floatFormatter(TextEditingValue oldValue, TextEditingValue newValue) {
+    if (widget.allowEmptyString && newValue.text == '') {
+      return newValue;
+    }
+    try {
+      final text = newValue.text;
+      if (text.length > widget.maxNumDigits) {
+        return oldValue;
+      }
+      if (text == '.') {
+        return TextEditingValue(text: '0.', selection: TextSelection.fromPosition(TextPosition(offset: 2)));
+      }
+      if (text.isNotEmpty) {
+        double value = double.parse(text);
+      }
+      return newValue;
+    } catch (e) {}
+    return oldValue;
   }
 
   @override
@@ -78,27 +157,8 @@ class _TableTextFieldState extends State<TableTextField> {
       style: TextStyle(fontSize: 11, fontFamily: 'NotoSans', color: widget.textColor),
       keyboardType: const TextInputType.numberWithOptions(decimal: false, signed: false),
       inputFormatters: [
-        FilteringTextInputFormatter.allow(RegExp(r"[0-9]")),
-        TextInputFormatter.withFunction((oldValue, newValue) {
-          if (widget.allowEmptyString && newValue.text == '') {
-            return newValue;
-          }
-          try {
-            final text = newValue.text;
-            if (text.startsWith('0')) {
-              return oldValue;
-            }
-            if (text.length > widget.maxNumDigits) {
-              return oldValue;
-            }
-            if (text.isNotEmpty) {
-              // try parse
-              int value = int.parse(text);
-            }
-            return newValue;
-          } catch (e) {}
-          return oldValue;
-        }),
+        FilteringTextInputFormatter.allow(RegExp(widget.floatingPoint ? r"[0-9.]" : r"[0-9]")),
+        TextInputFormatter.withFunction(widget.floatingPoint ? floatFormatter : intFormatter),
       ],
     );
   }
