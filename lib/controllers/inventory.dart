e4f91ed48d26994ae2a37834d40ad8ecbbe2f905
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-05-02 19:22:08 -0400

    idk.

--- lib/controllers/inventory.dart
+++
@@ -1,8 +0,0 @@
-import 'package:EveIndy/models/inventory.dart';
-import 'package:flutter/material.dart';
-
-class InventoryController with ChangeNotifier {
-  final _inventory = Inventory.empty();
-
-  Inventory getInventoryCopy() => Inventory.cloneOf(_inventory);
-}
