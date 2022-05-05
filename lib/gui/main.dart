02423dc92b54525637121dabb7186ca03fb7d392
Bradley Bauer <gazingatstars.o7@gmail.com>
2022-05-04 20:59:07 -0400

    MyApp no longer needs to be Stateful.

--- lib/gui/main.dart
+++ lib/gui/main.dart
@@ -4,29 +4,15 @@ import 'package:provider/provider.dart';
 import 'my_theme.dart';
 import 'widgets/main.dart';
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    // theme.addListener(() {
-    //   setState(() {});
-    // });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<MyTheme>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'EveIndy',
+      title: 'EveReactor',
       home: const HomePage(),
       theme: theme.theme,
       themeMode: ThemeMode.light,
