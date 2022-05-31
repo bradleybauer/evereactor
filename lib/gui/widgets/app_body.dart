17e84d42a8aa31d066a0b4ffc8f0348df468e72c
caldaricitizen31341 <caldaricitizen31341@gmail.com>
2022-05-30 21:09:48 -0400

    Remove unused font.

--- lib/gui/widgets/app_body.dart
+++ lib/gui/widgets/app_body.dart
@@ -1,10 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'table_intermediates.dart';
-import 'summary_bar.dart';
 import 'table_inputs.dart';
 import 'table_targets.dart';
 import '../my_theme.dart';
